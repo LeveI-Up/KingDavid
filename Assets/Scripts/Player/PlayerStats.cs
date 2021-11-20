bb2b833f69d6c87fafb9cee48b48d0d11f8c19a8
Saar <saar9550@gmail.com>
2021-11-20 19:07:15 +0200

    PlayerStats Added

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    [SerializeField] string playerName;
+    [SerializeField] int playerLevel = 1;
+    [SerializeField] int currentXP;
+
+    [SerializeField] int maxHP = 100;
+    [SerializeField] int currentHP;
+
+    [SerializeField] int maxMana=30;
+    [SerializeField] int currnetMana;
+
+    [SerializeField] int defence;
+    [SerializeField] int dexterity;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
