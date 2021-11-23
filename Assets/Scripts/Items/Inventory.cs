33ea27d6288294d1e25c86a3d8b960828e9ffd3b
Saar <saar9550@gmail.com>
2021-11-23 13:00:38 +0200

    Create Inventory

---
+++ Assets/Scripts/Items/Inventory.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public static Inventory instance;
+
+    private List<ItemsManager> itemsList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        itemsList = new List<ItemsManager>();
+        Debug.Log("new list created");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void AddItems(ItemsManager item)
+    {
+        itemsList.Add(item);
+    }
+}
