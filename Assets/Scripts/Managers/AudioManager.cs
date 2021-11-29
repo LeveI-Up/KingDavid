16febcf818a859d967e09a30625dcc50c79249e8
Saar <saar9550@gmail.com>
2021-11-30 00:57:22 +0200

    Music and SFX added + Modify gitidnore

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+
+    [SerializeField] AudioSource[] backgraoundMusic, SFX;
+
+    public static AudioManager instance;
     // Start is called before the first frame update
     void Start()
     {
-        
+        instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            PlaySFX(3);
+        }
+    }
+
+    public void PlaySFX(int soundToPlay)
+    {
+        if(soundToPlay < SFX.Length)
+        {
+            SFX[soundToPlay].Play();
+        }
     }
 }
