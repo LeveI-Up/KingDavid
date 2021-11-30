a36a29b9557e3f4f6c5e0e1356b7e1a57663d2b4
Saar <saar9550@gmail.com>
2021-11-30 11:04:17 +0200

    Created MainMenu

---
+++ Assets/Scripts/Managers/MainMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+
+    [SerializeField] string newGameScene;
+    [SerializeField] GameObject continueButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("Player_Pos_X"))
+        {
+            continueButton.SetActive(true);
+        }
+        else
+        {
+            continueButton.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NewGameButton()
+    {
+        SceneManager.LoadScene(newGameScene);
+    }
+    
+    public void ExitButton()
+    {
+        Application.Quit();
+        Debug.Log("Quit the game");
+    }
+}
