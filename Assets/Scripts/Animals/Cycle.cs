35b051a53fd9c54ed42034ff26b3635c558318ba
uriyashemla <uriyashem1994@gmail.com>
2022-03-15 10:35:55 +0200

    Revert "Revert "farm update + animation""
    This reverts commit eaab46987682c1d4492899d27dca16f91714ff6c.

---
+++ Assets/Scripts/Animals/Cycle.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Cycle : MonoBehaviour
+{
+    [SerializeField] float gizmoRadius = 0.2f;  // for gizmos of patrol points
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        for (int i = 0; i < transform.childCount; ++i)
+        {
+            Gizmos.DrawSphere(transform.GetChild(i).position, gizmoRadius);
+            Gizmos.DrawLine(transform.GetChild(i).position, transform.GetChild((i + 1) % transform.childCount).position);
+        }
+    }
+}
