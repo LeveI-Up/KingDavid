b72d3e2be9ffdeedb08b94bd29fb198766e3eb00
Saar <saar9550@gmail.com>
2022-03-28 14:51:32 +0300

    Added Quest Panel

---
+++ Assets/Scripts/Quests/QuestPanel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class QuestPanel : MonoBehaviour
+{
+
+    public static QuestPanel instance;
+
+    [SerializeField] TextMeshProUGUI questText;
+    [SerializeField] GameObject questScreen;
+
+
+    [SerializeField] bool markQuestComplete;
+    [SerializeField] string questToMarkName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (questScreen.activeInHierarchy)
+            {
+                questScreen.SetActive(false);
+            }
+            else
+            {
+                questText.text = "ילש םייחב יתשגפש לודג יכה הנוז ןבה התא הירוא";
+                questScreen.SetActive(true);
+            }
+        }
+
+    }
+    public void CloseQuestScreen()
+    {
+        questScreen.SetActive(false);
+    }
+
+
+}
